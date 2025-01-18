f505f70fc3c632b239cfb38fa0eda58c8d2a9135
Sam Valentino <sammyvalentino7@gmail.com>
2025-01-18 12:48:26 -0500

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 8;
+let num4 = 2;
+let mod = num3 / num4;
+
+let max = Math.max(1, 5, 10, 20);
